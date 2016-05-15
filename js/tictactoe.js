262ec20302eb9e58ef3b224d4b3a1d7f1f7bf96f
shasta28 <jessahafer-zdral@Jessas-MacBook-Pro.local>
2016-05-15 13:54:54 -0400

    add grid helper methods

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -8,5 +8,22 @@ var grid = {
 				$(row).append('<div class="cell" data-coord="'+x+'-'+y+'" data-mark=""></div>');
 			}
 		}
+	},
+	getCell: function (coord) {
+		return $('.cell[data-coord='+coord[0]+'-'+coord[1]+']');
+	},
+	getCoord: function(cell) {
+		var coords = cell.attr("data-coord").split("-");
+		$.each(coords, function(i, value) {
+			coords[i] = parseInt(value);
+		});
+		return coords;
+	},
+	cellIsEmpty: function(cell) {
+		return cell.attr("data-mark") == "" && cell.html() == "";
+	},
+	coordIsEmpty: function(coord) {
+		var cell = getCell(coord);
+		return cellIsEmpty(cell);
 	}
 }
